3fcda9a2f5f0ff4f256c0e8b9e102594c781a119
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-26 15:20:12 +0530

    new concept of #private get / set the properties

--- classes/1index.js
+++ classes/1index.js
@@ -26,6 +26,56 @@ class Bank {
 const my = new Bank('shani',100)
 
 
+// charactor
+
+class Charactor {
+  #health;
+  constructor (name, type, power){
+     this.name = name
+     this.type = type
+     this.#health = 200 
+     this.power = power
+  };
+// get - getter function - for properties(both private/public) how its value is can be get
+  get health(){
+    return this.#health;
+  }
+
+  // set - setter function - for properties(both private/public) how its value can be set/modified
+  set health (value) {
+     throw new Error('what is this you can not change health directly')
+  }
+
+  attack(target) {
+    target.#health -= this.power
+    return `${this.name} attack on ${target.name} now the health of ${target.name} is ${target.#health}`
+  }
+
+  heal (target){
+     this.#health += target
+
+     return `${this.name} is healed by ${target} now his health is ${this.#health}`
+  }
+   
+
+}
+
+const hero = new Charactor('hero',' hero' , 50);
+const villen = new Charactor('villen', 'villen', 70);
+
+console.log(hero, villen);
+console.log(hero.attack(villen));
+console.log(villen.attack(hero));
+console.log(villen.heal(50));
+console.log(hero.health);
+console.log(villen.health);
+
+
+
+
+
+
+
 
 
 
