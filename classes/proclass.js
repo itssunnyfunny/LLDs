e82670ef4aac5dcd5ba8c479b8050f7624e9bbe1
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-30 14:15:55 +0530

    inplementation of the class and its properties

--- classes/proclass.js
+++ classes/proclass.js
@@ -1,16 +1,55 @@
 // classes
+// blueprint for creating objects
+
+class calculate {
+  add(a,b){
+   return a*b;
+  }
+}
 
 // contructor
+// automatically called when the new Class is intialized
+class animal {
+    constructor (name, species){
+        this.name = name
+        this.species = species
+    }
+
+    info(){
+        return `${this.name} is species of ${this.species}`
+    }
+}
 
 // properties
+ // this.name and this.species is properties of that object
 
 // methods
+// add( ) and info() is methods for there own objects
 
 // setter & getter
+//getter and setter is a specific methods which is look like properties 
+// and specifically used for geting and updating any properties( public & private)
+ class student {
+    constructor (name, rollNo){
+        this.name = name
+        this.rollNo = rollNo
+    }
 
-// inheritance - extends
+    get rollNo(){
+        return this.rollNo
+    }
+
+    set rollNo(newNum){
+         this.rollNo = newNum
+    }
+ }
+
+// inheritance - extends 
+// using the logic of a class and extends its properties and methods to build out new class
 
 // super()
+// is is specically used for using any logic which parent have to use in the child class
+
 
 // static
 
