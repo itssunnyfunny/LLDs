40b9052980bf3a738d6090686b3023c928a3b2ff
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-11 11:09:41 +0530

    testing whole app via container in index.ts

--- solidPrjects/ParkingLotSystem/src/index.ts
+++ solidPrjects/ParkingLotSystem/src/index.ts
@@ -1,3 +1,15 @@
+import IoCContainer from "./containers/IoCContainer";
+import { Car } from "./models/Car";
 
 
- 
+const container = IoCContainer.getInstance();
+
+try {
+   const spotId  = container.parkingService.park(new Car("CAR-123"));
+   console.log(`Parked at spot ${spotId}`);
+   container.parkingService.unpark(spotId);
+   console.log(`Unparked from  spot ${spotId}`);
+} catch (error) {
+    console.error(error.message);
+    
+}
