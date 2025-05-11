487957f38ac54a9ab310af7808f7a228962086f5
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-11 12:04:55 +0530

    adding LoanService for borrowing the books

---
+++ solidPrjects/LibraryManagementSystem/src/repositories/LoanService.ts
@@ -0,0 +1,25 @@
+import { IRepository } from "./IRepository";
+import { Borrowable } from "../models/Barrowable";
+
+
+export class LoanService {
+    constructor(private repository: IRepository<Borrowable>) {}
+
+    borrowItem(id: string): void {
+        const item = this.repository.getById(id);
+        if (!item) {
+            throw new Error("Item not found");
+        }
+        item.borrow();
+        this.repository.save(item);
+    }
+
+    returnItem(id: string): void {
+        const item = this.repository.getById(id);
+        if (!item) {
+            throw new Error("Item not found");
+        }
+        item.return();
+        this.repository.save(item);
+    }
+}
